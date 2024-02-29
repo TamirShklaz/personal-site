4e897c3e76a47bdd1b7c34293d1e977c5e31856d
tamirshklaz <shklazt@gmail.com>
2024-02-29 17:14:59 +0700

    github pages 404 routing action

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import {resolve} from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     base: '/',
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, "index.html"),
-                404: resolve(__dirname, "public/404.html"),
-            },
-        },
-    }
 })
